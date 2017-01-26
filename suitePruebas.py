6a4a6e957cffb764f7b6e7794133877a9089fa98
daniel <daniel@daniel-desktop>
2017-01-26 13:26:41 -0400

    Renombramiento de Tarifa.py a calcularPrecio.py para reflejar mejor el caracter de la funcion.
    Creacion del archivo suitePruebas.py

---
+++ suitePruebas.py
@@ -0,0 +1,18 @@
+'''
+Created on Jan 26, 2017
+
+@author: daniel
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
