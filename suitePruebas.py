ad9f89dc160193c988bde17dafa3c532ec1ebb6a
daniel <daniel@daniel-desktop>
2017-01-26 13:42:39 -0400

    Creacion de la prueba 'testOnlyWeekday'

--- suitePruebas.py
+++ suitePruebas.py
@@ -4,14 +4,20 @@ Created on Jan 26, 2017
 @author: daniel
 '''
 import unittest
+import datetime
+from calcularPrecio import *
+
+class TestServicio(unittest.TestCase):
 
 
-class Test(unittest.TestCase):
-
-
-    def testName(self):
-        pass
-
+    def testOnlyWeekday(self):
+        
+        fecha1 = datetime.datetime(2016,1,23,13,0,0)
+        fecha2 = datetime.datetime(2016,1,26,13,0,0)
+        tiempoDeServicio = [fecha1,fecha2]
+        tarifa = Tarifa(5.0,6.0)
+        monto = calcularPrecio(tarifa,tiempoDeServicio)
+        self.assertEqual(monto,480,"Fallo en la prueba testOnlyWeekday")
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
